d998e9d4a398c9ecef0496ca015bc1cc806e35ed
EagleEyes <qquocviet2602@gmail.com>
2020-09-03 18:44:28 +0700

    ...

--- DBPhanQuyen.sql
+++ DBPhanQuyen.sql
@@ -3,27 +3,32 @@ create table Menu (
 	,[Name] nvarchar(255)
 )
 go
+
 create table Permission (
 	Id int primary key identity(1,1)
 	,[Name] nvarchar(255)
 )
+GO
 
 create table MenuPermission (
 	Id int primary key identity(1,1)
 	,MenuId int references Menu(Id)
 	,PermissionId int references Permission(Id)
 )
+GO
 
 create table [Role] (
 	Id int primary key identity(1,1)
 	,[Name] nvarchar(255)
 )
+Go
 
 create table RolePermission (
 	Id int primary key identity(1,1)
 	,MenuPermissionId int references MenuPermission(Id)
 	,RoleId int references [Role](Id)
 )
+GO
 
 create table EmployeeRole (
 	Id int primary key identity(1,1)
@@ -31,6 +36,7 @@ create table EmployeeRole (
 	,EmployeeId int references [Employee](Id)
 )
 GO
+
 insert into Menu values 
  (N'Releaser')
 ,(N'Book')
@@ -40,14 +46,44 @@ insert into Menu values
 ,(N'Reader')
 ,(N'BookTitle')
 GO
+
 insert into Permission values 
- (N'Create')
+ (N'View')
+,(N'Create')
 ,(N'Edit')
 ,(N'Delete')
 ,(N'Import')
 ,(N'Export')
-,(N'View')
 GO
+
 insert into MenuPermission values 
  (1,1)
 ,(1,2)
+,(1,3)
+,(1,4)
+GO
+
+insert into [Role] values 
+ (N'Admin')
+,(N'Manager')
+,(N'Staff')
+GO
+
+insert into RolePermission values 
+ (1,1)
+,(2,1)
+,(3,1)
+,(4,1)
+,(1,2)
+,(2,2)
+,(1,3)
+GO
+
+insert into EmployeeRole values 
+ (1,1)
+,(2,2)
+,(3,3)
+,(3,11)
+GO
+
+select * from EmployeeRole
